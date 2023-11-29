2ca5a3cffc526c919f2954347c5371d1f589cb79
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-11-29 10:38:38 +0200

    train n tires

---
+++ Tire.php
@@ -0,0 +1,16 @@
+<?php
+class Tire {
+    public $size;
+    public $type;
+    private $quality;
+
+    public function __construct($size, $type, $quality) {
+        $this->size = $size;
+        $this->type = $type;
+        $this->quality = $quality;
+    }
+
+    public function __destruct() {
+        echo "Boom!";
+    }
+}
--- index.php
+++ index.php
@@ -3,6 +3,8 @@ include 'Animal.php';
 include 'Dog.php';
 include 'Giraffe.php';
 include 'Car.php';
+include 'Train.php';
+include 'Tire.php';
 
 $animals = [
     new Giraffe("Fred", 4),
@@ -15,5 +17,17 @@ $myCar2 = new Car("BMW", 568);
 $myCar2->increaseMileage(240);
 
 Car::makeNoise();
+
+$myTrain1 = new Train("Caltrain", 900, 5482);
+$myTrain2 = new Train("CN", 1000, 142);
+
+$myTrain1->increaseMileage(240);
+
+Train::makeNoise();
+
+$myTire1 = new Tire("Goodyear", "Winter", 69);
+
+$myTire1->brand = "nvm";
+$myTire1->type = "Summer";
 
 
