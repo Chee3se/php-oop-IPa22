a3e8d85b871c05473e01452ae9e9d4e88d3b0f20
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-11-29 10:52:54 +0200

    Titles

--- index.php
+++ index.php
@@ -6,10 +6,14 @@ include 'Car.php';
 include 'Train.php';
 include 'Tire.php';
 
+//Animals
+
 $animals = [
     new Giraffe("Fred", 4),
     new Dog("Rufus", 6)
 ];
+
+//Car
 
 $myCar1 = new Car("Volvo", 1936);
 $myCar2 = new Car("BMW", 568);
@@ -18,12 +22,16 @@ $myCar2->increaseMileage(240);
 
 Car::makeNoise();
 
+//Train
+
 $myTrain1 = new Train("Caltrain", 900, 5482);
 $myTrain2 = new Train("CN", 1000, 142);
 
 $myTrain1->increaseMileage(240);
 
 Train::makeNoise();
+
+//Tire
 
 $myTire1 = new Tire(30, "Winter", 69);
 
