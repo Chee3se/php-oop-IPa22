dae52bbc977f1466938fbde9416a818e9bd1f416
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-11-29 11:33:19 +0200

    yes

--- Tire.php
+++ Tire.php
@@ -1,14 +1,11 @@
 <?php
 class Tire {
-    public $size;
-    public $type;
-    private $quality;
 
-    public function __construct($size, $type, $quality) {
-        $this->size = $size;
-        $this->type = $type;
-        $this->quality = $quality;
-    }
+    public function __construct(
+        public $size,
+        public $type,
+        private $quality
+    ) {}
 
     public function __destruct() {
         echo "Boom!<br>";
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+//Including
+
 include 'Animal.php';
 include 'Dog.php';
 include 'Giraffe.php';
